9b91d0cbcca38454d89436245c3d343375488b2c
Nabeen Tiwaree <90331517+Iyamnabeen@users.noreply.github.com>
2022-04-22 23:32:23 +0545

    💟

---
+++ HackerRank/warmup/02.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+    "fmt"
+unc main() {
+    var a, ok, sam, i
+    fmt.Scan(&a)
+    for i := 0; i < a; i++ {
+    fmt.Scan(&ok)
+    sam += ok
+     }
+     fmt.Println(sam)
+        }
